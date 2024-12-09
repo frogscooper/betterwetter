987ba464efeaf01c8445485e0d2a6711ba7aa8cc
Froggy <frogscooper@gmail.com>
2024-12-09 13:41:23 -0500

    taking a break, figuring out some stuf about pandas series and whatnot

--- betterwetter.py
+++ betterwetter.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import pgeocode
+import pandas
 import os
 from dotenv import load_dotenv
 
 country = pgeocode.Nominatim('us')
 zip_code = int(input("Which ZIP code area are you in now? > "))
-location_data = country.query_postal_code(zip_code)
+zip_location = country.query_postal_code(zip_code)
 
+location_data = str(pandas.Series(zip_location, index = ["place_name", "state_code"]))
+print(location_data)
