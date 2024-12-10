ef1ac81c0bb581bb3138a7feeec130061fa9a7a8
Froggy <frogscooper@gmail.com>
2024-12-09 20:21:38 -0500

    finally figured out how to format it correctly for the API :D so happy

--- betterwetter.py
+++ betterwetter.py
@@ -10,4 +10,7 @@ zip_code = int(input("Which ZIP code area are you in now? > "))
 zip_location = country.query_postal_code(zip_code)
 
 location_data = str(pandas.Series(zip_location, index = ["place_name", "state_code"]))
-print(location_data)
+location_data = location_data.split()
+del location_data[0], location_data[1], location_data[2:]
+filtered_location_data = ', '.join(location_data)
+print(filtered_location_data)
