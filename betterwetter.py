6f4fb693be3f7394d29159d49667778ceaa5721f
Froggy <frogscooper@gmail.com>
2024-12-13 18:40:20 -0500

    It works! The only thing left to do now is convert the JSON data into a more readable format.

--- betterwetter.py
+++ betterwetter.py
@@ -10,16 +10,16 @@ country = pgeocode.Nominatim('us')
 zip_code = int(input("Which ZIP code area are you in now? > "))
 zip_location = country.query_postal_code(zip_code)
 
-##Shitty code that filters the data received from pgeocode into just (City, State)
-location_data = str(pandas.Series(zip_location, index = ["place_name", "state_code"]))
+##Shitty code that filters the data received from pgeocode into just Latitude and Longitude
+location_data = str(pandas.Series(zip_location, index = ["latitude", "longitude"]))
 location_data = location_data.split()
 del location_data[0], location_data[1], location_data[2:]
-city_name = ','.join(location_data)
 
-
+##Queries OpenWeatherMap for the weather
 load_dotenv()
 api_key = os.environ.get("Your_API_Key")
-base_weather_url = "http://api.openweathermap.org/data/2.5/weather?"
-complete_weather_url = base_weather_url + "appid=" + api_key + "&q=" + city_name
+base_weather_url = str(f"http://api.openweathermap.org/data/2.5/weather?")
+complete_weather_url = base_weather_url + "lat=" + location_data[0] + "&lon=" + location_data[1] + "&appid=" + api_key
 response = requests.get(complete_weather_url)
-print(response)
+weather = response.json()
+print(weather)
